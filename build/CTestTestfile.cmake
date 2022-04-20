3c150b51964c015c11c10997b4bd49470fa6a00e
Phi Thanh Danh <phithanhdanh2001@gmail.com>
2022-04-20 09:45:32 +0700

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/phithanhdanh/rplidar_ws/src
+# Build directory: /home/phithanhdanh/rplidar_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("openslam_gmapping")
+subdirs("slam_gmapping/slam_gmapping")
+subdirs("map_server")
+subdirs("rplidar_ros")
+subdirs("slam_gmapping/gmapping")
---
+++ build/openslam_gmapping/CMakeFiles/rdk2carmen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/phithanhdanh/rplidar_ws/devel/lib/openslam_gmapping/rdk2carmen"
+  "/home/phithanhdanh/rplidar_ws/devel/lib/openslam_gmapping/rdk2carmen.pdb"
+  "CMakeFiles/rdk2carmen.dir/log/rdk2carmen.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rdk2carmen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
